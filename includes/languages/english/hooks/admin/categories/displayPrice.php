faa44e29d66f127ba2852c24392fb66c6f5a1583
Dennis <denzel@kds-online.com>
2020-12-22 10:38:41 +0100

    Create displayPrice.php

---
+++ includes/languages/english/hooks/admin/categories/displayPrice.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  Copyright (c) 2020, Denzel
+
+  This work is licensed under a 
+  Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+
+  You should have received a copy of the license along with this work.
+  If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+*/
+
+define('TEXT_PRODUCT_DISPLAY_PRICE_LABEL', 'Display Price:');
+define('TEXT_PRODUCT_DISPLAY_PRICE_YES', 'Yes');
+define('TEXT_PRODUCT_DISPLAY_PRICE_NO', 'No');
+   
